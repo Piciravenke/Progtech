a6040ef52dbffea1e4a75f0a478c7313d9b51be5
piciravenke <piciravenke@gmail.com>
2016-05-20 23:46:07 +0200

    tesztek befejezve + adatb progress

--- PrtSzamologep/src/main/java/hu/unideb/inf/maven/prtszamologep/Calculator.java
+++ PrtSzamologep/src/main/java/hu/unideb/inf/maven/prtszamologep/Calculator.java
@@ -8,31 +8,40 @@ package hu.unideb.inf.maven.prtszamologep;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author miki
- */
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Calculator")
 public class Calculator {
-    private List<String> expression;
+    
+    @Id
+    @Column(name = "id")
+    int id;
+    @Column(name = "expression")
+    private String expression;
+    @Column(name = "result")
     private String result;
+    @Column(name = "method")
     private String method;
+    @Column(name = "date")
     private String date;
 
     public Calculator() {
     }
 
-    public Calculator(List<String> expression, String result, String method, String date) {
+    public Calculator(int id,String expression, String result, String method, String date) {
+        this.id = id;
         this.expression = expression;
         this.result = result;
         this.method = method;
         this.date = date;
     }
 
-    public List<String> getExpression() {
+    public String getExpression() {
         return expression;
     }
 
-    public void setExpression(List<String> expression) {
+    public void setExpression(String expression) {
         this.expression = expression;
     }
 
