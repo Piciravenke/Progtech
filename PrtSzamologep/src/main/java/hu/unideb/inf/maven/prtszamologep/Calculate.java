17a33adf583e6826b687dbd5c8cffd3e46a4bd5f
Miklós Lukács <piciravenke@gmail.com>
2016-05-21 11:04:33 +0200

    license

--- PrtSzamologep/src/main/java/hu/unideb/inf/maven/prtszamologep/Calculate.java
+++ PrtSzamologep/src/main/java/hu/unideb/inf/maven/prtszamologep/Calculate.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hu.unideb.inf.maven.prtszamologep;
 
 import java.math.BigInteger;
@@ -11,7 +6,8 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- *
+ * 
+ * 
  * @author miki
  */
 public class Calculate {
